0d3fdf4de16a3df3e42b58f46e33ae090e46d00f
jabra98 <34109209+jabra98@users.noreply.github.com>
2021-01-27 01:09:37 +0100

    add more constraints on the templated types

---
+++ searching/include/searching/binary_search.hpp
@@ -0,0 +1,43 @@
+#ifndef JAB_BINARY_SEARCH_HPP
+#define JAB_BINARY_SEARCH_HPP
+
+#include <iterator>
+#include <concepts>
+
+namespace jab::searching
+{
+// returns an iterator to the searched object.
+// if the object wasn't found, the end iterator is returned
+// Requirements: the sequence [begin, end) must be sorted
+// such that *begin <= *end
+
+template<typename T, typename ItV = std::iter_value_t<T>> 
+    requires(std::random_access_iterator<T> && std::totally_ordered<ItV>
+        && std::same_as<std::remove_cvref_t<ItV>, std::remove_cvref_t<std::iter_value_t<T>>>)
+T binary_search(T begin, T end, ItV&& obj)
+{
+    T low = begin; 
+    T hi = end-1;
+    while (low <= hi)
+    {
+        T mid = low+std::distance(low, hi)/2;
+
+        if (*mid > obj)
+        {
+            hi = mid-1;
+        }
+        else if (*mid < obj)
+        {
+            low = mid+1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    return end;
+}
+
+} // namespace jab::searching
+
+#endif // JAB_BINARY_SEARCH_HPP
---
+++ searching/include/searching/linear_search.hpp
@@ -0,0 +1,27 @@
+#ifndef JAB_LINEAR_SEARCH_HPP
+#define JAB_LINEAR_SEARCH_HPP
+
+#include <iterator>
+#include <concepts>
+
+namespace jab::searching
+{
+
+// returns an iterator to the searched object.
+// if the object wasn't found, the end iterator is returned
+template<typename T, typename ItV = std::iter_value_t<T>> 
+    requires(std::forward_iterator<T> && std::equality_comparable<ItV>
+        && std::same_as<std::remove_cvref_t<ItV>, std::remove_cvref_t<std::iter_value_t<T>>>)
+T linear_search(T begin, T end, ItV&& obj)
+{
+    for (auto i = begin; i!=end; ++i)
+    {
+        if (*i == obj)
+            return i;
+    }
+    return end;
+}
+
+} // namespace jab::searching
+
+#endif // JAB_LINEAR_SEARCH_HPP
